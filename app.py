9a3d63df70660d9bc5cec7435eecb484308cdc0c
Genar Trias Ortiz <genar@acs.li>
2016-01-12 12:03:37 +0100

    Joining server and app files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, Response, request
 from werkzeug import secure_filename
 import pdfkit
+
+import cherrypy
+
 app = Flask(__name__)
 
 tmpfolder = "/tmp/"
@@ -46,4 +49,30 @@ def handle_request(config):
     return pdf
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    # Mount the application
+    cherrypy.tree.graft(app, "/")
+
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 80
+    server.thread_pool = 30
+
+    # For SSL Support
+    # server.ssl_module            = 'pyopenssl'
+    # server.ssl_certificate       = 'ssl/certificate.crt'
+    # server.ssl_private_key       = 'ssl/private.key'
+    # server.ssl_certificate_chain = 'ssl/bundle.crt'
+
+    # Subscribe this server
+    server.subscribe()
+
+    # Start the server engine (Option 1 *and* 2)
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
